b7c5c6d35f2bc80c12261983dd9da9fe95b70ca0
Marie Seindal <marie@seindal.dk>
2020-02-20 16:47:57 +0100

    fjerne fuck

--- src/comboBreaker.java
+++ src/comboBreaker.java
@@ -20,16 +20,22 @@ public class comboBreaker {
 
         for (int i = 0; i < input.length(); i++) {
             moves[i] = input.charAt(i);
-            System.out.println(moves[i]);
+//            System.out.println(moves[i]);
         }
 // Logikken
-        for (int i=0; i<len; i++)
-            if (combo(moves,i)) {
-            System.out.print("F");
-            i+=3; //Hvis den laver en combo, så skal den ikke kigge på 4 næste
+
+        for (int i=0; i<len; i++) {
+
+            if (i < len - 3 && combo(moves, i)) {
+                System.out.print("F");
+                i += 3; //Hvis den laver en combo, så skal den ikke kigge på 4 næste
             }
-        else
-            System.out.print(counter(moves[i]));
+            else
+                System.out.print(counter(moves[i]));
+            if (i != len-1)
+                System.out.print(" ");
+
+        }
 
     }
 
@@ -37,13 +43,14 @@ public class comboBreaker {
         int startingIndex=index;
 
         for (int k=0 ; k<4; k++) { // finder ens bogstaver
-            if (moves[index] == moves[index+1]) // tjekker 1 med 2, 2 med 3 og 3 med 4.
+//            System.out.println(index +"  |  " + startingIndex);
+            if (index <= startingIndex+2 && moves[index] == moves[index+1]) // tjekker 1 med 2, 2 med 3 og 3 med 4.
                 return false;
 
-            else if (moves[index] == moves[index+2] && index <= startingIndex+1) // Tjekker 1 me 3 og 2 med 4
+            else if (index <= startingIndex+1 && moves[index] == moves[index+2]) // Tjekker 1 me 3 og 2 med 4
                 return false;
 
-            else if (moves[index] == moves[index+3] && index == startingIndex) // tjekker 1 med 4
+            else if (startingIndex == index && moves[index] == moves[index+3]) // tjekker 1 med 4
                 return false;
 
             else
@@ -60,8 +67,10 @@ public class comboBreaker {
             return 'E';
         else if (a=='R')
             return 'C';
-        else
+        else if (a =='S')
             return 'S';
+        else
+            return ' ';
 
     }
 
