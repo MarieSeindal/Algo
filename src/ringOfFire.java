e4bcefa4e13da51fe10cbb8acd701ab6e08b7782
Marie Seindal <marie@seindal.dk>
2020-02-20 09:02:27 +0100

    Ring of fire, input succefuld

--- src/ringOfFire.java
+++ src/ringOfFire.java
@@ -1,5 +1,33 @@
-public class ringOfFire {
+import java.util.Scanner;
 
+public class ringOfFire {
+    public static void main(String[] args) {
+
+        //input = int N,C,K // K= slut antal elever, N = students, C = eliminationNr
+
+        Scanner sc = new Scanner(System.in);
+        int n,c,k;
+
+        n = sc.nextInt();
+        c = sc.nextInt();
+        k = sc.nextInt();
+
+        //Lav array af integers, aka elever
+        int[] elever = new int[n];
+        for (int i=0; i<n; i++ ) {
+            elever[i] = i+1;
+            System.out.println(elever[i]);
+        }
+
+    }
+    public static int dead(int c, int n, int k, int[] elever){
+        int counter = c;
+        for (int i=0; i<n-k; i++)
+
+
+
+
+    }
 
 
 }
