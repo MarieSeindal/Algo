a8d9a60fbaf6f5763e7bca5697b2b6bd29ed0864
AngelofDeath666 <khejser@hotmail.com>
2020-02-21 13:46:07 +0100

    blin it is good

--- src/ringOfFire.java
+++ src/ringOfFire.java
@@ -13,29 +13,41 @@ public class ringOfFire {
         k = sc.nextInt(); // Number of survivors
 
         //Lav array af integers, aka elever
-        int[] elever = new int[n];
+        boolean[] students = new boolean[n];
         for (int i=0; i<n; i++ ) {
-            elever[i] = i+1;
-//            System.out.println(elever[i]);
+            students[i] = true;
+//            System.out.println(students[i]);
         }
 
     }
-    public static void dead(int c, int n, int k, int[] elever){ //Ændre de elever i arrayet, som er døde
+    public static void dead(int c, int n, int k, boolean[] students){ //Ændre de elever i arrayet, som er døde
 
         int count = c;
+
 
         for (int i=0; i<n-k; i++) { //loop gennem total student array
 
             // Vi har en tæller, som tæller ned
 
-            if (elever[i] == elever.length-1){ //Hvis tællingen går ud over arrayets kant
+            if (i == students.length-1){ //Hvis tællingen går ud over arrayets kant
+                i = 0;
 
-
-
-            }else{
-                
             }
+
+            if (!students[i]) {
+                continue;
+
+
+            } else if (count == 1) {
+                students[i] = false;
+                count = c;
+
+            }
+
+            count--;
         }
+
+
         
     }
 
