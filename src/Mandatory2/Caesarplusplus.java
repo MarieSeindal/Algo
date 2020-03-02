62eded771c7f1ced1d6c098f461941f005a08c7f
Marie Seindal <marie@seindal.dk>
2020-03-02 11:31:07 +0100

    input

--- src/Mandatory2/Caesarplusplus.java
+++ src/Mandatory2/Caesarplusplus.java
@@ -1,8 +1,35 @@
 package Mandatory2;
 
+import java.io.*;
+import java.util.*;
+
 public class Caesarplusplus {
 
     public static void main(String[] args) {
+
+
+        // input til output
+
+        Scanner sc = new Scanner(System.in);
+        Stack<Character> stack = new Stack<Character>();
+
+        // tager den første integer og læser den
+        String length = sc.nextLine();
+        int len = Integer.parseInt(length);
+
+        String input = sc.nextLine();
+        input = input.replaceAll("\\s", "");
+
+
+
+
+
+
+
+
+
+
+
 
     }
 
