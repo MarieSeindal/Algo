b66a846ebd33fc9912b2e1f927e00b73c00cc9f7
Marie Seindal <marie@seindal.dk>
2020-03-02 12:42:00 +0100

    Virker ikke med tal der skal poppes

--- src/Mandatory2/Caesarplusplus.java
+++ src/Mandatory2/Caesarplusplus.java
@@ -7,9 +7,7 @@ public class Caesarplusplus {
 
     public static void main(String[] args) {
 
-
-        // input til output
-
+// input til output
         Scanner sc = new Scanner(System.in);
         Stack<Character> stack = new Stack<Character>();
 
@@ -23,28 +21,44 @@ public class Caesarplusplus {
 
         // genere stacken
         for (int i=0; i<len; i++){
-            if (input.charAt(i) == 94) {
+
+            boolean yesInt = false;
+            int a;
+
+            try {
+                a= Character.getNumericValue(input.charAt(i));
+                yesInt = true;
+            }catch (Exception e){
+                System.out.println("Not an int");
+            }
+
+
+            if (input.charAt(i) == 94) { //Tjekker for pop  (^)
+
+
+                if (yesInt == true){
+                    System.out.println("Move numbers: " + a);
+                }
+
                 char pop = stack.pop(); // todo
                 System.out.println("Pop: " +  pop); //todo
+
                 continue;
             }
 
             stack.push(input.charAt(i));
 
-            if (input.charAt())
+
+
+
+
+
+
 
 
         }
 
         // metode for at printe stacken // todo
-
-
-
-
-
-
-
-
 
 
     }
