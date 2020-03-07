45eaab878ef30db7555b57701037bfbfc10d1478
AngelofDeath666 <khejser@hotmail.com>
2020-03-07 15:59:18 +0100

    Wrote push method and created a alphabet array :D

--- src/Mandatory2/Caesarplusplus.java
+++ src/Mandatory2/Caesarplusplus.java
@@ -19,6 +19,19 @@ public class Caesarplusplus {
         String input = sc.nextLine();
         input = input.replaceAll("\\s", "");
 
+        // Pushing every character of the string into the stack
+        char[] ch = input.toCharArray();
+        for (int i = 0; i < input.length(); i++) {
+            stack.push(ch[i]);
+        }
+
+        //Creating a alphabet array
+        char[] alpha = new char[26];
+        for(int i = 0; i < 26; i++){
+            alpha[i] = (char)(65 + i);
+        }
+
+
         // genere stacken
         for (int i=0; i<len; i++){
 
@@ -26,7 +39,7 @@ public class Caesarplusplus {
             int a;
 
             try {
-                a= Character.getNumericValue(input.charAt(i));
+                a = Character.getNumericValue(input.charAt(i));
                 yesInt = true;
             }catch (Exception e){
                 System.out.println("Not an int");
@@ -53,7 +66,7 @@ public class Caesarplusplus {
     }
     // metode for at printe stacken // todo
     public static void stackPrint (char [] stack) {
-        for (int p = 0 ; i < n; i++) {
+        for (int p = 0 ; i < ; i++) {
 
         }
     }
